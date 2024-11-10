00aab17bc6a53a8a43dd0f4265f8a67345ae998e
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-11-10 22:52:42 +0100

    chore: change nail types description by the costumer. StaticImage imports instead of src. remake completely the mobile version of the component.

--- components/homepage/nails-types.tsx
+++ components/homepage/nails-types.tsx
@@ -1,78 +1,63 @@
 "use client";
 
-import { useState, useEffect, useRef, Dispatch, SetStateAction } from "react";
-import Image from "next/image";
-import { motion, AnimatePresence, useInView } from "framer-motion";
+import { useState, useEffect, useRef, SetStateAction } from "react";
+import Image, { StaticImageData } from "next/image";
+import { motion, useInView } from "framer-motion";
 import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
 import { cn } from "@/lib/utils";
 import gsap from "gsap";
 import { useTheme } from "next-themes";
-import FallingPetalsBackground from "../NailsSection/petals-falling";
 import { useGSAP } from "@gsap/react";
+import { Eye, EyeOff } from "lucide-react";
+import MurettoImage from "@/public/unghie/muretto.webp";
+import FrenchBabyBoomer from "@/public/unghie/french-baby-boomer.webp";
+import TrendNails from "@/public/unghie/trend-nails.webp";
+import UnghieNaturali from "@/public/unghie/unghie-naturali.webp";
 
 gsap.registerPlugin(ScrollToPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
-interface Service {
+interface nailType {
   name: string;
-  image: string;
-  note: string;
+  image: StaticImageData;
   description: string;
 }
-
-interface PaginationProps {
-  length: number;
-  currentPosition: number;
-  setCurrentPosition: Dispatch<SetStateAction<number>>;
-}
-
-interface SectionProps {
-  service: Service;
-  index: number;
-}
-
-const services: Service[] = [
+const nailsTypes: nailType[] = [
   {
-    name: "Unghie muretto",
-    image: "/unghie/muretto.webp",
-    note: "Distinguiti con le unghie muretto, un’arte unica che unisce stile e originalità!",
+    name: "Unghie a muretto",
+    image: MurettoImage,
     description:
-      "I nostri esperti onicotecnici creano design straordinari che catturano l’attenzione e riflettono la tua personalità. Clicca per scoprire come dare vita a unghie che raccontano la tua storia!",
+      "Un French che è parte della ricostruzione, e non un disegno, è una tecnica di ricostruzione delle unghie con cui la nail artist non si limita a disegnare la smile line delle unghie con il gel bianco, o di altro colore, sopra l'unghia già ricostruita ma crea la smile line direttamente in struttura.",
   },
   {
     name: "French & Babyboomer",
-    image: "/unghie/french-baby-boomer.webp",
-    note: "Eleganza e raffinatezza per le tue unghie",
+    image: FrenchBabyBoomer,
     description:
-      "Rinnova il tuo look con French o Babyboomer! Questi stili classici esaltano la bellezza delle tue unghie con eleganza. Clicca per scoprire come ottenere un finish impeccabile!",
+      "Il french manicure rientra tra le prime tecniche di manicure che negli ultimi tempi ha riavuto una piccola rivincita su altri più vivaci e sfiziosi mentre, il baby boomer è l’ultima tendenza del manicure, molto sobrio e naturale come il precedente, ma con una sfumatura delicata",
   },
   {
     name: "Trend Nails",
-    image: "/unghie/trend-nails.webp",
-    note: "Unghie all’avanguardia che esprimono la tua personalità",
+    image: TrendNails,
     description:
-      "Scopri le unghie in trend e fai brillare il tuo stile! Colori vibranti e forme audaci per un look che esprime chi sei. Clicca e trasforma le tue unghie in vere opere d’arte!",
+      "Continui aggiornamenti sui trend del momento. Inspo continui per farti sognare ed essere sempre alla moda",
   },
   {
     name: "Unghie naturali",
-    image: "/unghie/unghie-naturali.webp",
-    note: "Eleganza e versatilità con tonalità nude",
+    image: UnghieNaturali,
     description:
-      "Svela la tua bellezza autentica con unghie naturali e nude, perfette per ogni occasione. Clicca per scoprire come un look minimalista possa esaltare la tua femminilità!",
+      "Svela la tua bellezza con unghie naturali, nude, semplici ed eleganti per ogni occasione. Esalta la tua femminilità con i miei servizi.",
   },
   {
     name: "Sprint Nails",
-    image: "/materials.jpg",
-    note: "Energia e vitalità per le tue unghie",
+    image: UnghieNaturali,
     description:
-      "Dai un tocco di energia alle tue unghie con lo stile Sprint! Colori audaci e design dinamici che esprimono movimento e creatività. Clicca e rendi le tue unghie un inno alla vivacità!",
+      "Fantasie, disegni, personalità, brillantini, animals… sono solo alcune delle parole chiave delle Sprint Nails.. sbizzarriamoci per avere le unghie dei tuoi sogni.",
   },
   {
-    name: "Acrygel e gel",
-    image: "/materials.jpg",
-    note: "Durata e versatilità per unghie sempre alla moda",
+    name: "Card Nails",
+    image: UnghieNaturali,
     description:
-      "Sperimenta la forza delle unghie in Acrigel e gel! Questi trattamenti offrono resistenza e un look impeccabile che dura nel tempo. Clicca e scopri il look perfetto per te!",
+      "Ultimo arrivato ma ha subito rapito i cuori di tanti: tu scegli il prezzo e la fortuna sceglierà le unghie al posto tuo!",
   },
 ];
 
@@ -81,6 +66,12 @@ const NailsTypes: React.FC = () => {
   const [currentSection, setCurrentSection] = useState(0);
   const sectionRef = useRef<HTMLDivElement[] | null>(null);
   const isInView = useInView(containerRef, { amount: 0.95 });
+  const [nailType, setService] = useState(nailsTypes[currentSection]);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setService(nailsTypes[currentSection]);
+  }, [currentSection]);
 
   useEffect(() => {
     const container = containerRef.current;
@@ -112,144 +103,144 @@ const NailsTypes: React.FC = () => {
   }, [currentSection]);
 
   return (
-    <>
+    <section
+      ref={containerRef}
+      className={cn(
+        "w-full h-[600px] lg:h-[1000px] max-h-screen min-h-[600px] z-20 rounded-2xl mx-auto snap-y snap-mandatory scrollbar-hide",
+        isInView ? "overflow-y-scroll" : "overflow-hidden",
+        "bg-gray-300 dark:bg-black/20 bg-opacity-70",
+        "bg-cover bg-center bg-no-repeat"
+      )}
+      style={{
+        backgroundImage: `url(${nailType.image})`,
+      }}
+    >
       <section
-        ref={containerRef}
         className={cn(
-          "w-full h-[600px] lg:h-[1000px] max-h-screen min-h-[600px] z-20 rounded-2xl mx-auto snap-y snap-mandatory scrollbar-hide",
-          isInView ? "overflow-y-scroll" : "overflow-hidden",
-          "bg-gray-300 dark:bg-black/20 bg-opacity-70"
+          "absolute bottom-8 left-8 z-20",
+          "flex flex-col items-center justify-center gap-10",
+          // queries
+          "max-md:px-1 max-md:bottom-32"
         )}
       >
-        <Image
-          src="/hand-nail-red-2.webp"
-          alt="hand flowers"
-          width={800}
-          height={600}
+        {/* Pagination */}
+        <div className="flex flex-col gap-4">
+          {[...Array(nailsTypes.length)].map((_, index) => (
+            <div
+              key={index}
+              onClick={() => setCurrentSection(index)}
+              className={cn(
+                "flex shrink-0 rounded-sm overflow-hidden transition-all duration-200 cursor-pointer",
+                index === currentSection
+                  ? "w-8 h-8 max-md:w-3 max-md:h-3 bg-rose dark:bg-softWhite-50"
+                  : "w-8 h-4 max-md:w-3 max-md:h-3 bg-softWhite-50 dark:bg-rose"
+              )}
+            ></div>
+          ))}
+        </div>
+        {/*scollabile */}
+        <motion.div
+          style={{
+            opacity: isInView ? 1 : 0,
+            y: isInView ? 0 : 20,
+          }}
           className={cn(
-            "absolute max-lg:-top-10 -top-36 mx-auto z-0 mask-gradient blur-lg"
-          )}
-        />
-
-        <section
-          className={cn(
-            "absolute bottom-5 left-5 z-10",
-            "flex flex-col items-center justify-center gap-10",
-            // queries
-            "max-md:px-1 max-md:bottom-32"
+            "hero-link-to-base rounded-full flex items-start justify-center max-md:w-4 w-8 h-16",
+            "border-2 border-white bg-transparent",
+            "transition-all duration-200 ease-linear"
           )}
         >
-          {/* Pagination */}
-          <div className="flex flex-col gap-4">
-            {[...Array(services.length)].map((_, index) => (
-              <div
-                key={index}
-                onClick={() => setCurrentSection(index)}
-                className={cn(
-                  "flex shrink-0 rounded-sm overflow-hidden transition-all duration-200 cursor-pointer",
-                  index === currentSection
-                    ? "w-8 h-8 max-md:w-3 max-md:h-3 bg-rose dark:bg-softWhite-50"
-                    : "w-8 h-4 max-md:w-3 max-md:h-3 bg-white dark:bg-gray-700/30"
-                )}
-              ></div>
-            ))}
+          <div className="bright">
+            <div className="size-4 max-md:size-2 rounded-full bg-white animate-bounce-more"></div>
           </div>
-          {/*scollabile */}
-          <motion.div
-            style={{
-              opacity: isInView ? 1 : 0,
-              y: isInView ? 0 : 20,
-            }}
-            className={cn(
-              "hero-link-to-base rounded-full flex items-start justify-center max-md:w-4 w-8 h-16",
-              "border-2 border-white bg-transparent",
-              "transition-all duration-200 ease-linear"
-            )}
-          >
-            <div className="bright">
-              <div className="size-4 max-md:size-2 rounded-full bg-white animate-bounce-more"></div>
-            </div>
-          </motion.div>
-        </section>
-
-        {/* Services */}
-        {services.map((service, index) => (
-          <div
-            key={index}
-            ref={(el: HTMLDivElement | null) => {
-              if (el && sectionRef.current) {
-                sectionRef.current[index] = el as HTMLDivElement;
-              }
-            }}
-          >
-            <Section service={service} index={index} />
-          </div>
-        ))}
+        </motion.div>
       </section>
-    </>
+
+      {/* Services */}
+      {nailsTypes.map((nailType, index) => (
+        <NailType
+          key={index}
+          style={{
+            backgroundColor: theme === "dark" ? "bg-gray-200" : "bg-gray-900",
+          }}
+          ref={(el: HTMLDivElement | null) => {
+            if (el && sectionRef.current) {
+              sectionRef.current[index] = el as HTMLDivElement;
+            }
+          }}
+          nailType={nailType}
+          index={index}
+          currentSection={currentSection}
+        />
+      ))}
+    </section>
   );
 };
 
-const Section: React.FC<SectionProps> = ({ service, index }: SectionProps) => {
-  const { theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+const NailType: React.FC<any> = ({ nailType, currentSection, index }: any) => {
+  const [viewImage, setViewImage] = useState<boolean>(false);
 
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) {
-    return null;
-  }
+  useEffect(() => {
+    if (currentSection !== index) {
+      setViewImage(false);
+    }
+  }, [currentSection]);
 
   return (
-    <motion.div
+    <div
       className={cn(
-        "h-[600px] lg:h-[1000px] w-full flex items-center justify-center snap-start overflow-hidden"
+        "relative",
+        "z-10 mx-auto max-md:px-20",
+        `bg-cover bg-center bg-no-repeat`,
+        "flex items-center justify-center gap-20",
+        "h-[600px] lg:h-[1000px] w-full snap-start overflow-hidden"
       )}
-      style={{
-        backgroundColor: theme === "dark" ? "bg-gray-200" : "bg-gray-900",
-      }}
     >
-      <div className="z-10 md:flex md:flex-row w-full h-full mx-auto max-md:px-10">
-        <div
-          className={cn(
-            "max-md:w-full w-1/2 h-full mb-8 md:mb-0 flex flex-col items-center justify-center "
-            // index % 2 !== 0 && "order-1"
-          )}
-        >
-          <h2 className="text-5xl md:text-6xl font-extrabold mb-4 text-white dark:text-white drop-shadow-md shadow-black">
-            {service.name}
-          </h2>
-          <p className="text-xl text-gray-700 dark:text-gray-300 mb-6 max-w-md text-pretty">
-            {service.description}
-          </p>
-          <button className="bg-white dark:bg-gray-800 max-md:w-full text-[rgb(183,110,121)] dark:text-[rgb(255,228,225)] py-3 px-8 rounded-full text-lg font-semibold hover:bg-opacity-90 dark:hover:bg-opacity-80 transition-colors shadow-lg">
-            Prenota Ora
-          </button>
-        </div>
-
-        <motion.figure
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          animate={{ y: 300, opacity: 0 }}
-          className={cn(
-            "max-md:hidden w-1/2 h-screen relative"
-            // index % 2 === 0 && "order-1"
-          )}
-        >
-          <Image
-            src={service.image}
-            alt={service.name}
-            loading="lazy"
-            blurDataURL={service.image}
-            fill
-            className="aspect-auto"
-          />
-          <figcaption className="absolute bottom-4 left-4 text-white text-xl font-semibold z-20">
-            {service.note}
-          </figcaption>
-        </motion.figure>
-      </div>
-    </motion.div>
+      <Image
+        src={nailType.image}
+        placeholder="blur"
+        blurDataURL={nailType.image.blurDataURL}
+        alt={nailType.name}
+        objectFit="cover"
+        width={1280}
+        height={720}
+        className={cn(
+          "absolute lg:hidden h-[600px] lg:h-[1000px] w-full overflow-hidden -z-10",
+          "transition-all transform-gpu duration-300 ease-in-out",
+          viewImage ? "" : "blur-2xl "
+        )}
+      />
+      <section
+        className={cn(
+          "w-full lg:w-1/2 h-full",
+          "place-content-center grid",
+          "space-y-2 lg:space-y-8",
+          viewImage ? "hidden" : ""
+        )}
+      >
+        <h2 className="text-3xl md:text-5xl font-extrabold mb-4 text-roseGold-light dark:text-roseGold-accent">
+          {nailType.name}
+        </h2>
+        <p className="text-md lg:text-xl text-softWhite-50 mb-6 text-pretty">
+          {nailType.description}
+        </p>
+      </section>
+      <button
+        onClick={() => setViewImage(!viewImage)}
+        className="lg:hidden flex flex-row flex-nowrap group gap-2 items-center justify-center w-fit bg-black/20 backdrop-blur-lg text-[rgb(255,228,225)] p-3 rounded-full text-lg font-semibold hover:bg-opacity-90 dark:hover:bg-opacity-80 transition-colors shadow-lg absolute bottom-8 z-10 left-1/2 -translate-x-1/2"
+      >
+        {viewImage ? <EyeOff className="size-5" /> : <Eye className="size-5" />}
+      </button>
+      <Image
+        src={nailType.image}
+        alt={nailType.name}
+        loading="lazy"
+        placeholder="blur"
+        width={500}
+        height={500}
+        className="max-lg:hidden w-1/2 h-screen"
+      />
+    </div>
   );
 };
 
