7d96a33cc585d26b9cd5e5966fa6f9bb1bcbf268
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-12-03 00:27:40 +0100

    config: next config moved to ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,44 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 import { config } from "dotenv";
 
 config();
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   experimental: {
     ppr: true,
   },
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'images.unsplash.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'seo-heist.s3.amazonaws.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'github.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'ansubkhan.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'utfs.io',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'images.opencollective.com',
+      port: '',
+      pathname: '/**'
+    }]
+  }
 };
-
-export default nextConfig;
+module.exports = nextConfig;
