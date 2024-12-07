71b95c13a327dd35addc6320456b8eb281397725
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-12-07 23:38:56 +0100

    fix: z-index of scroll animation

--- components/homepage/scroll-animation.tsx
+++ components/homepage/scroll-animation.tsx
@@ -21,7 +21,7 @@ function ScrollIcon() {
     });
   }, []);
   return (
-    <div className="hero-link-to-base absolute p-3  bottom-0 right-5 lg:right-auto mb-5 rounded-full flex items-start justify-center w-[30px] h-[60px] border-2 border-roseGold dark:border-white bg-transparent z-50">
+    <div className="hero-link-to-base absolute p-3  bottom-0 right-5 lg:right-auto mb-5 rounded-full flex items-start justify-center w-[30px] h-[60px] border-2 border-roseGold dark:border-white bg-transparent z-20">
       <div className="bright">
         <div className="size-3 rounded-full bg-roseGold dark:bg-white animate-bounce-more"></div>
       </div>
