b62b61ff0c8bc90c396b3dfe18f354ab4e7f4db7
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-12-11 15:10:54 +0100

    fix: section margins

--- lib/actions/reviews.ts
+++ lib/actions/reviews.ts
@@ -1,48 +1,49 @@
-"use server"
+"use server";
 
-import { eq } from 'drizzle-orm'
-import db from '../db/drizzle'
-import { reviews as reviewsdb } from '../db/schema'
-import { revalidatePath } from 'next/cache'
+import { eq } from 'drizzle-orm';
+import db from '../db/drizzle';
+import { reviews as reviewsdb } from '../db/schema';
+import { revalidatePath } from 'next/cache';
 
 export async function getReviews() {
     try {
-        const reviews = await db.select().from(reviewsdb)
+        const reviews = await db.select().from(reviewsdb);
         return reviews.map(review => ({
             body: review.body,
             name: review.name,
             rating: review.rating,
-            date: review.createdAt!.toISOString().split('T')[0]
-        }))
+            date: review.created_at!.toISOString().split('T')[0]
+        }));
     } catch (error) {
-        console.error('Errore nel recupero delle recensioni:', error)
-        throw new Error('Impossibile recuperare le recensioni')
+        console.error('Errore nel recupero delle recensioni:', error);
+        throw new Error('Impossibile recuperare le recensioni.');
     }
 }
 
 export async function submitReview(formData: FormData) {
-    const name = formData.get('name') as string
-    const body = formData.get('body') as string
-    const rating = formData.get('rating') as string
-    const isAnonymous = formData.get('isAnonymous') === 'on'
+    const name = formData.get('name') as string || 'Anonim@';
+    const body = formData.get('body') as string;
+    const rating = formData.get('rating') as string;
+    const isAnonymous = formData.get('isAnonymous') === 'on';
 
-    console.log(name, body, rating, isAnonymous)
+    console.log(name, body, rating, isAnonymous);
 
-    if (!body || Number(rating) < 1 || Number(rating) > 5 || !name) {
-        return { error: 'Inserisci un nome, una recensione e una valutazione tra 1 e 5' }
+    if (!body || Number(rating) < 1 || Number(rating) > 5) {
+        return { error: 'Inserisci una recensione valida e una valutazione tra 1 e 5.' };
     }
 
     try {
-        await db.insert(reviewsdb).values({ // TODO: fix the name field
-            name: isAnonymous ? 'Anonymous' : name || 'Anonymous',
+        await db.insert(reviewsdb).values({
+            name: isAnonymous ? 'Anonim@' : name,
             body,
             rating,
-            createdAt: new Date() as unknown as Date,
-        })
-        revalidatePath('/')
-        return { success: true }
+            status: 'idle',
+            created_at: new Date() as unknown as Date,
+        });
+        revalidatePath('/');
+        return { success: true, message: 'La tua recensione è stata inviata con successo.' };
     } catch (error) {
-        console.error('Failed to submit review:', error)
-        return { error: 'Failed to submit review' }
+        console.error('Errore durante l\'invio della recensione:', error);
+        return { error: 'Errore interno: impossibile inviare la recensione.' };
     }
 }
