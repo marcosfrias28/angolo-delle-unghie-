cde90a33d768dda0c665d9478ade82116df79471
Marcos  Frias Molina <marcofrias0828@gmail.com>
2025-01-10 14:56:05 -0500

    add: Dashboard page first design template

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,28 +1,90 @@
-import AdminAppointments from "@/components/appointments/admin-appointments";
 import StandardHeading from "@/components/generic/standard-heading";
-import AboutMe from "@/components/homepage/about-me";
+import ReviewCard from "@/components/homepage/review-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SectionWrapper } from "@/components/wrapper/section-wrapper";
+import { getReviews } from "@/lib/actions/reviews";
 import { getUser } from "@/lib/db/queries";
-import { User } from "@/lib/db/schema";
+import { Review, User } from "@/lib/db/schema";
+import { cn } from "@/lib/utils";
 import { redirect } from "next/navigation";
 
 async function App() {
-  // const user = (await getUser()) as User;
+  const user = (await getUser()) as User;
+  const reviews = (await getReviews()) as Review[];
 
-  // if (!user) {
-  //   redirect("/login");
-  // }
+  if (!user) {
+    redirect("/login");
+  }
+  console.log(reviews);
+  const isAdmin = user.role === "admin";
 
   return (
     <SectionWrapper>
       <StandardHeading
-        title="Conosciamoci meglio!"
-        description="Prima di tutto, ti racconto di me."
+        title={
+          isAdmin
+            ? "Pannello di Amministrazione - Recensioni"
+            : "Le tue recensioni"
+        }
+        description={
+          isAdmin
+            ? "Questo pannello ti permette di visualizzare e gestire le recensioni dei tuoi clienti."
+            : "Visualizza e gestisci le recensioni lasciate"
+        }
         position="center"
       />
-      <AboutMe />
+
+      {isAdmin ? <AdminDashboard reviews={reviews} /> : <CostumerDashboard />}
     </SectionWrapper>
   );
 }
 
 export default App;
+
+const CostumerDashboard = () => {
+  return (
+    <Tabs className="flex flex-col items-center justify-center">
+      <TabsList defaultValue="idle" color="rgb(228, 183, 180)">
+        <TabsTrigger value="all">Le mie recensioni</TabsTrigger>
+      </TabsList>
+      <TabsContent value="all">
+        <h1>Tutte le recensioni</h1>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+const AdminDashboard = ({ reviews }: { reviews: Review[] }) => {
+  return (
+    <Tabs className="flex flex-col items-center justify-center">
+      <TabsList defaultValue="idle" color="rgb(228, 183, 180)">
+        <TabsTrigger value="all">Tutte</TabsTrigger>
+        <TabsTrigger value="idle">In Attesa</TabsTrigger>
+        <TabsTrigger value="accepted">Accettate</TabsTrigger>
+        <TabsTrigger value="rejected">Non accettate</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        className={cn(
+          "grid",
+          "gap-y-10 gap-x-5",
+          "max-md:grid-cols-1 lg:grid-cols-2",
+          "grid-rows-auto"
+        )}
+        value="all"
+      >
+        {reviews.map((review) => (
+          <ReviewCard {...review} key={review.id} />
+        ))}
+      </TabsContent>
+      <TabsContent value="idle">
+        <h1>Recensioni in attesa</h1>
+      </TabsContent>
+      <TabsContent value="accepted">
+        <h1>Recensioni accettate</h1>
+      </TabsContent>
+      <TabsContent value="rejected">
+        <h1>Recensioni non accettate</h1>
+      </TabsContent>
+    </Tabs>
+  );
+};
